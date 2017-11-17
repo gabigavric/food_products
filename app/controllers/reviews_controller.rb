fb9e3f0f3306889a73b24decb977762805ae3890
gabrijela <gabigavric@gmail.com>
2017-11-17 12:53:05 -0800

    change out controller reviews from lessons tasks to review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,41 @@ class ReviewsController < ApplicationController
    @product = Product.find(params[:product_id])
    @review = @product.reviews.new
  end
+
+ def create
+   @product = Product.find(params[:product_id])
+   @review = @product.reviews.new(review_params)
+   if @review.save
+     redirect_to product_path(@review.product)
+   else
+     render :new
+   end
+end
+
+ def edit
+   @product = Product.find(params[:product_id])
+   @review = @product.reviews.find(params[:id])
+ end
+
+ def update
+   @review= Review.find(params[:id])
+   @product = Product.find(params[:product_id])
+   if @review.update(review_params)
+     redirect_to product_path(@review.product)
+   else
+     render :edit
+   end
+ end
+
+ def destroy
+   @review = Review.find(params[:id])
+   @review.destroy
+   redirect_to product_path(@review.product)
+ end
+
+ private
+   def review_params
+     params.require(:review).permit(:author, :content_body, :rating)
+   end
+
 end
