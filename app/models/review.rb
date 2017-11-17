a06cc515357dacbbc38a0d0351fc3e304f6660f0
gabrijela <gabigavric@gmail.com>
2017-11-17 12:25:38 -0800

    spec passing for validating prsence of author

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,7 +1,7 @@
 class Review < ActiveRecord::Base
   belongs_to :product
   validates :product_id, :presence => true
-  # validates :author, :presence => true
+  validates :author, :presence => true
   # validates :content_body, {:presence => true, :length => { :minimum => 50, :maximum => 250 }}
   # validates :rating, :presence => true
 end
