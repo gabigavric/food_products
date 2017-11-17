aef427d1d4fe139ff825ecc922da7f5affd87f61
gabrijela <gabigavric@gmail.com>
2017-11-17 12:10:53 -0800

    spec passing for validating origin

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -4,5 +4,5 @@ describe Product do
   it { should have_many :reviews }
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
-  # it { should validate_presence_of :origin }
+  it { should validate_presence_of :origin }
 end
