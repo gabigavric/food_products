2bc245fad7b1c0e906034b1783171c41878171d4
gabrijela <gabigavric@gmail.com>
2017-11-17 14:50:11 -0800

    add scope for most reviewed

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,4 +3,11 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true
   validates :cost, :presence => true
   validates :origin, :presence => true
+
+  scope :most_reviewed, -> {(
+    select("products.name, products.cost, products.origin, products.id, products.created_at, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    )}
 end
