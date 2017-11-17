07f258a0dad3ee905ef9a0953c09bbe3749d37b1
gabrijela <gabigavric@gmail.com>
2017-11-17 14:59:28 -0800

    spec passing for most viewd scope

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -6,3 +6,15 @@ describe Product do
   it { should validate_presence_of :cost }
   it { should validate_presence_of :origin }
 end
+
+describe '#most_reviewed' do
+    it "should return the product with the most reviews" do
+      Product.create(name: "name", cost: 1, origin: "origin")
+      product = Product.create(name: "First Series Harry Potter Book", cost: 10, origin: "England")
+      4.times do
+        product.reviews.create(author: "author", content_body: "review", rating: 5)
+      end
+      most_popular_product = Product.most_reviewed
+      expect(most_popular_product[0].name).to eq product.name
+    end
+end
