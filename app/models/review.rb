039d4f87f5b37aefe6be884bdcf528e48dd40156
gabrijela <gabigavric@gmail.com>
2017-11-17 12:24:38 -0800

    spec passing for reviews belonging to product and having product id

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,7 @@
 class Review < ActiveRecord::Base
-  belongs_to :prodcut
-  validates :author, :presence => true
+  belongs_to :product
+  validates :product_id, :presence => true
+  # validates :author, :presence => true
   # validates :content_body, {:presence => true, :length => { :minimum => 50, :maximum => 250 }}
   # validates :rating, :presence => true
 end
