cae08705a62b67e8519645a82571d72e8588d8d3
gabrijela <gabigavric@gmail.com>
2017-11-17 16:20:11 -0800

    add faker to products and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,21 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+
+50.times do
+  product = Product.create!(
+  name: Faker::Commerce.name,
+  cost: Faker::Commerce.price,
+  origin: Faker::Address.country
+ )
+end
+
+250.times do
+  review = Review.create!(
+    author: Faker::HarryPotter.character,
+    content_body: Faker::HarryPotter.quote,
+    rating: Faker::Number.between(1,5),
+    product_id: product_id
+end
+
+  p "Created #{Product.count} products"
+  p "Created #{Review.count} reviews"
