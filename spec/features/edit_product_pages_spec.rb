92626a72d8b1eaf03c5b47a6404e1208a74f4114
gabrijela <gabigavric@gmail.com>
2017-11-23 23:21:28 -0800

    spec test for errors popping  up for empty edit product input field

--- spec/features/edit_product_pages_spec.rb
+++ spec/features/edit_product_pages_spec.rb
@@ -10,4 +10,15 @@ describe "editing a product" do
     visit product_path(product)
     expect(page).to have_content 'Enders Game'
   end
+
+
+  it "gives error when no description is entered" do
+    product = Product.create(:name => 'Harry Potter', :cost => '15', :origin => 'England')
+    visit product_path(product)
+    click_link 'Edit Product'
+    fill_in 'Name', :with => ''
+    click_on 'Update Product'
+    expect(page).to have_content 'errors'
+  end
+
 end
