fe43aff142bf1852329f5600f3ef60df0e9707c3
gabrijela <gabigavric@gmail.com>
2017-11-23 22:37:28 -0800

    add missing paranthases

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,16 @@ Product.destroy_all
 Review.destroy_all
 
 50.times do
-  product = Product.create!(
-  name: Faker::Commerce.name,
-  cost: Faker::Commerce.price,
-  origin: Faker::Address.country
- )
+  Product.create!( name: Faker::Commerce.name,
+                   cost: Faker::Commerce.price,
+                   origin: Faker::Address.country )
 end
 
 250.times do
-  review = Review.create!(
-    author: Faker::HarryPotter.character,
-    content_body: Faker::HarryPotter.quote,
-    rating: Faker::Number.between(1,5),
-    product_id: product_id
+  Review.create!( author: Faker::HarryPotter.character,
+                  content_body: Faker::HarryPotter.quote,
+                  rating: Faker::Number.between(1,5),
+                  product_id: product_id )
 end
 
   p "Created #{Product.count} products"
