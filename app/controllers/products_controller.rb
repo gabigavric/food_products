1cc5e9d5dc3bb7057bf4867957c877bcffcdfcc7
gabrijela <gabigavric@gmail.com>
2017-11-23 18:03:42 -0800

    move the end so the rest of the product controller defenitions are included in the end tag fixed error

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,34 +1,50 @@
 class ProductsController < ApplicationController
+
   def index
     @products = Product.all
   end
-end
 
-def show
-  @product = Product.find(params[:id])
-end
-
-def new
-  @product = Product.new
-end
-
-def create
-  @product = Product.new(product_params)
-  if @product.save
-    flash[:notice] = "Product successfully added!"
-    redirect_to products_path(@product)
-  else
-    render :new
-  end
-end
-
-def destroy
+  def show
     @product = Product.find(params[:id])
-    @product.destroy
-    redirect_to lists_path
-end
-
-private
-  def product_params
-    params.require(:product).permit(:name, :cost, :origin)
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      flash[:notice] = "Product successfully added!"
+      redirect_to products_path(@product)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+      @product = Product.find(params[:id])
+      @product.destroy
+      redirect_to lists_path
+  end
+
+  def edit
+     @product = Product.find(params[:id])
+  end
+
+   def update
+     @product = Product.find(params[:id])
+     if @product.update(product_params)
+       flash[:success] = "Update was successful"
+       redirect_to products_path
+     else
+       flash[:danger] = "Was not able to update"
+       render :edit
+     end
+   end
+
+  private
+    def product_params
+      params.require(:product).permit(:name, :cost, :origin)
+    end
+end
