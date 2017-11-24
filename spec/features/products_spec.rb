d3b77799796f255ecec80b92a63836cc6703b0ff
gabrijela <gabigavric@gmail.com>
2017-11-23 23:29:38 -0800

    spec test for landing page showing most reviewed

--- spec/features/products_spec.rb
+++ spec/features/products_spec.rb
@@ -1,42 +1,12 @@
-# require 'rails_helper'
-#
-# describe "the add a list process" do
-#   it "adds a new list" do
-#     visit lists_path
-#     click_link 'New List'
-#     fill_in 'Name', :with => 'Home stuff'
-#     click_on 'Create List'
-#     expect(page).to have_content 'Lists'
-#   end
-#
-#   it "gives error when no name is entered" do
-#     visit new_list_path
-#     click_on 'Create List'
-#     expect(page).to have_content 'errors'
-#   end
-# end
+require 'rails_helper'
 
-# 
-# require 'rails_helper'
-#
-# describe "the edit a task process" do
-#   it "edits a task" do
-#     list = List.create(:name => 'Home stuff')
-#     task = Task.create(:description => 'Wash the dishes', :list_id => list.id)
-#     visit list_path(list)
-#     click_on 'Edit'
-#     fill_in 'Description', :with => 'Wash the dog'
-#     click_on 'Update Task'
-#     expect(page).to have_content 'Wash the dog'
-#   end
-#
-#   it "gives error when no description is entered" do
-#     list = List.create(:name => 'Home stuff')
-#     task = Task.create(:description => 'Wash the dishes', :list_id => list.id)
-#     visit list_path(list)
-#     click_on 'Edit'
-#     fill_in 'Description', :with => ''
-#     click_button 'Update Task'
-#     expect(page).to have_content 'errors'
-#   end
-# end
+  describe 'landing page' do
+    it 'show the product with the most reviews' do
+      product1 = Product.create(:name => 'Animal Farm', :cost => '12', :origin => 'USA')
+      product2 = Product.create(:name => 'The Count of Monte Cristo', :cost => '13', :origin => 'France')
+      product3 = Product.create(:name => 'Dorothy Must Die', :cost => '11', :origin => 'USA')
+      review1 = Review.create(:author => 'Gabi', :content_body => 'Gruesome', :rating => 3, :product_id => product3.id)
+      visit '/'
+      expect(page).to have_content 'Dorothy Must Die';
+  end
+end
